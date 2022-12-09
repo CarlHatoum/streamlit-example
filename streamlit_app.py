6c49e794a741b7e2449f9c50334b4afe2c24f6fc
Carl Hatoum <32509445+CarlHatoum@users.noreply.github.com>
2022-12-09 20:18:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,27 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+# Créez la page d'accueil d'Amazon
+st.title("Bienvenue sur Amazon")
+st.markdown("**Explorez notre large sélection de produits**")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Affichez les catégories de produits
+st.sidebar.title("Catégories de produits")
+categories = ["Livres", "Électronique", "Vêtements", "Jouets", "Meubles"]
+selected_category = st.sidebar.selectbox("Choisissez une catégorie", categories)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Affichez les produits dans la catégorie sélectionnée
+if selected_category == "Livres":
+    st.subheader("Livres")
+    st.markdown("Affichage des livres disponibles dans cette catégorie")
+elif selected_category == "Électronique":
+    st.subheader("Électronique")
+    st.markdown("Affichage des produits électroniques disponibles dans cette catégorie")
+elif selected_category == "Vêtements":
+    st.subheader("Vêtements")
+    st.markdown("Affichage des vêtements disponibles dans cette catégorie")
+elif selected_category == "Jouets":
+    st.subheader("Jouets")
+    st.markdown("Affichage des jouets disponibles dans cette catégorie")
+else:
+    st.subheader("Meubles")
+    st.markdown("Affichage des meubles disponibles dans cette catégorie")
